cfa48019c7852b05cf295efea45c2d84fc96d94e
trentriek <trent1riek@gmail.com>
2023-08-31 01:08:10 -0500

    Added Blog link to website. Fixed Script that removed Nicepage watermark

--- LastLineRemove.sh
+++ LastLineRemove.sh
@@ -1,10 +1,10 @@
 
-yourfilenames=`ls -R ./*.html`
-#yourfilenames=`find . -name *.html`
+#yourfilenames=`ls -R ./*.html`
+yourfilenames=`find . -type f -name "*.html"`
 for eachfile in $yourfilenames 
 do
     echo $eachfile
-    if grep -Fq "https://nicepage.com/website-templates" "$eachfile"
+    if grep -Fq "https://nicepage.com" "$eachfile"
     then 
     echo here
     head -n -13 $eachfile > temp.html ; mv temp.html $eachfile
